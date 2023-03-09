1a079603b63d13f069373cd2fd112702be79a9ec
Fakhri17 <fakhricoffe@gmail.com>
2023-03-09 20:42:51 +0700

    add: setup early in vue js

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg py-3">
+    <div class="container">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse border-bottom" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="#">
+          <img src="@/assets/logo-basic-ssc.png" alt="SSC IMG" class="logo-header img-fluid">
+        </a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item d-lg-none d-block">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item d-lg-none d-block">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item d-lg-none d-block">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+       
+      </div>
+    </div>
+  </nav>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,32 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <section class="my-5">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-5 col-12 mb-4 mb-lg-0">
+            <div class="text-center text-lg-start ms-0 ms-lg-5">
+              <h2 class="fw-ssc mb-0"><strong class="text-danger">S</strong>tudent</h2>
+              <h2 class="fw-ssc mb-0"><strong class="text-danger">S</strong>ervice</h2>
+              <h2 class="fw-ssc mb-0"><strong class="text-danger">C</strong>enter</h2>
+              <p class="text-muted fs-5 mb-4">Datang, Buat Tiket, Proses</p>
+              <a href="#layanan" class="btn btn-danger px-4 py-2">Lihat Layanan</a>
+            </div>
+          </div>
+          <div class="col-md-7 col-12">
+            <div>
+              <img src="@/assets/home-img-ssc.png" alt="" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="my-5">
+      <div class="container">
+       <div class="py-5">
+        <h3><span class="text-danger"><i class="bi bi-book-half me-2"></i>Panduan</span> Pengajuan Layanan</h3>
+       </div>
+      </div>
+    </section>
   </main>
 </template>
