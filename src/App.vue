7ff124fa7dd895fa2b0864279fb996a23be1b6c9
Fakhri17 <fakhricoffe@gmail.com>
2023-03-10 10:40:48 +0700

    feat: back to top

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
-<script setup>
-  // import { RouterLink, RouterView } from 'vue-router'
-  import Header from './components/Header.vue';
-</script>
+
 
 
 <template>
   <Header />
   <RouterView />
+  <BackToTop />
 </template>
 
+<script>
+  // import { RouterLink, RouterView } from 'vue-router'
+  import Header from './components/Header.vue'
+  import BackToTop from './components/BackToTop.vue'
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      BackToTop
+    }
+  }
+</script>
 <style scoped>
 
 </style>
---
+++ src/components/BackToTop.vue
@@ -0,0 +1,31 @@
+<template>
+   <transition name="fade">
+    <button @click="BackToTop()" type="button" class="btn btn-ssc btn-floating btn-lg" id="btn-back-to-top">
+      <i class="bi bi-arrow-up"></i>
+  </button>
+
+  </transition>
+</template>
+
+<script>
+export default {
+ 
+  methods: {
+    BackToTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    },
+    scrollFunction(){
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btn-back-to-top").style.display = "block";
+      } else {
+        document.getElementById("btn-back-to-top").style.display = "none";
+      }
+    },
+  },
+  mounted() {
+    
+    window.addEventListener('scroll', this.scrollFunction)
+  },
+}
+</script>
